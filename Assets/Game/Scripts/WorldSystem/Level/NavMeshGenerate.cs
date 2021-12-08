306c6a7a4797653f9c8e6a6bb9ced5250b103620
naumnek <naumnek@yandex.ru>
2021-12-08 23:44:20 +0300

    Error

--- Assets/Game/Scripts/WorldSystem/Level/NavMeshGenerate.cs
+++ Assets/Game/Scripts/WorldSystem/Level/NavMeshGenerate.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavMeshGenerate : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<NavMeshSurface>().
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
